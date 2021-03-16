d7906279a5bde65c2865fc2655b56e6024d27880
saltfish0905 <975285932@qq.com>
2021-03-16 22:26:40 +0800

    'with-gui'

--- Address_book.py
+++ Address_book.py
@@ -1,3 +1,5 @@
+import easygui as g
+import sys
 address_book={}
 def init():
     global address_book
@@ -16,42 +18,60 @@ def init():
     ui()
 
 def search():
-    name=input("请输入联系人姓名：")
+    name=g.enterbox(msg='请输入联系人姓名',title='查询界面')
     if address_book.__contains__(name):
-        print("%s : %s"%(name,address_book[name]))
+        result=name+':'+address_book[name]
+        g.msgbox(msg=result,title='查找成功')
     else :
-        print("查无此人")
+        g.msgbox(msg='查无此人',title='查找失败')
 
 def add():
     global address_book
-    name=str(input("请输入联系人姓名："))
+    name=g.enterbox(msg='请输入联系人姓名',title='新增界面')
     if address_book.__contains__(name):
-        print("您输入的姓名在通讯录中已存在-->%s:%s"%(name,address_book[name]))
-        choice=str(input('是否修改用户资料（y\\n):'))
+        result=name+':'+address_book[name]
+        g.msgbox('您输入的姓名在通讯录中已存在-->'+result)
+        choice=g.enterbox(msg='是否修改用户资料?(y\\n)')
         if choice =='y':
-            address_book[name]=str(input("请输入联系人电话："))
-            print("修改成功")
+            address_book[name]=g.enterbox(msg='请输入联系人电话')
+            g.msgbox('修改成功')
         else :
             pass
     else :
-        address_book[name]=str(input("请输入联系人电话："))
-        print('插入成功')
+        address_book[name]=g.enterbox(msg='请输入联系人电话')
+        g.msgbox('插入成功')
 
 def delete():
     global address_book
-    name=input("请输入联系人姓名：")
+    name=g.enterbox(msg='请输入联系人姓名',title='删除界面')
     if address_book.__contains__(name):
         del address_book[name]
-        print("删除成功")
+        g.msgbox('删除成功')
     else :
-        print("查无此人")
+        g.msgbox(msg='查无此人',title='查找失败')
 
 def save_file():
     f=open('address_book.txt','w',encoding='utf-8')
     for name,phone in address_book.items():
         f.writelines(name+':'+phone+'\n')
-
+        f.close()
 def ui():
+    msg='欢迎使用通讯录程序！'
+    title='通讯录'
+    choices=['1.查询联系人资料','2.插入新的联系人','3.删除已有联系人','4.退出通讯录程序']
+    while 1:
+        choice=g.choicebox(msg,title,choices)
+        if choice=='1.查询联系人资料':
+            search()
+        elif choice=='2.插入新的联系人':
+            add()
+        elif choice=='3.删除已有联系人':
+            delete()
+        elif choice=='4.退出通讯录程序':
+            g.msgbox('感谢使用',title='结束界面')
+            save_file()
+            sys.exit(0)
+'''def ui():
     print("|---欢迎进入通讯录程序---|")
     print("|---1.查询联系人资料---|")
     print("|---2.插入新的联系人---|")
@@ -70,6 +90,6 @@ def ui():
             break
         else :
             print("不是有效的指令")
-    save_file()
+    save_file()'''
 
 init()
